88220ed0fd271d35cde09a96666983119f2f62c5
danney <danney@macbook.local>
2019-11-02 16:36:51 +0100

    init

---
+++ src/main/java/com/danney/auth/danneyauth/DanneyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.danney.auth.danneyauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanneyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanneyAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danney/auth/danneyauth/DanneyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danney.auth.danneyauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanneyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
